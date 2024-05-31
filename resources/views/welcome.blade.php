6e31ff3e30cf56dba4c0f82f0fc8dab80466db90
adzaky <muhadlim17@gmail.com>
2024-05-31 16:41:42 +0800

    Update welcome page, remove remember me checkbox in login page, and cleanup unused routes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
                     </a>
                 </main>
 
-                <footer class="py-16 text-center text-sm text-white/70">
+                <footer class="py-16 text-center text-sm">
                     Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                 </footer>
             </div>
